e9a8b5810aebb6e8719579a7a95c7c57d400d254
Jules Roche <julesro42@gmail.com>
2025-01-12 21:18:10 +0100

    adding books by genre in template

--- src/Controller/GenreContollerController.php
+++ src/Controller/GenreContollerController.php
@@ -7,13 +7,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class GenreController extends AbstractController
+class GenreContollerController extends AbstractController
 {
     #[Route('/genre/{id}', name: 'app_genre_contoller')]
     public function index(Genre $genre): Response
     {
         return $this->render('genre_contoller/index.html.twig', [
-            'genre' => $genre
+            'genre' => $genre,
+            'books' => $genre->getBooks()
         ]);
     }
 }
