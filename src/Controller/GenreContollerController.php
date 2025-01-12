97c0999b8d8520ebef85acfee8cad2d625b00a55
Jules Roche <julesro42@gmail.com>
2025-01-12 18:14:13 +0100

    ajout genreController

---
+++ src/Controller/GenreContollerController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Genre;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class GenreContollerController extends AbstractController
+{
+    #[Route('/genre/{id}', name: 'app_genre_contoller')]
+    public function index(Genre $genre): Response
+    {
+        return $this->render('genre_contoller/index.html.twig', [
+            'genre' => $genre
+        ]);
+    }
+}
