27d744f0d4def20abd5c2cf9a384055df0ca2e41
Jules Roche <julesro42@gmail.com>
2025-01-12 17:54:36 +0100

    redirection user non connecté

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,6 +12,9 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(UserRepository $userRepository): Response
     {
+        if ($this->getUser() === null) {
+            return $this->redirectToRoute('app_login');
+        }
         $users = $userRepository->findAll();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
